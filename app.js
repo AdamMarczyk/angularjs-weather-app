6fa09f2531b94d8384c5e07e0c5f7ac250b22915
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-29 20:53:33 +0200

    show weather

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-weatherApp.controller('homeController', ['$scope', function ($scope) {
+weatherApp.config(function ($routeProvider) {
 
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeController',
+    })
+    .when('/', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController',
+    });
+
+});
+
+weatherApp.service('cityService', function () {
+
+  this.city = "New York, NY";
+
+});
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, $cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.watch('city', function () {
+    cityService.city = $scope.city;
+
+  });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function ($scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function ($scope, $resource, $cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.weatherAPI = $resource("http://api.com", {
+    callback: "JSON_CALLBACK"
+  }, { get: { method: "JSONP" } });
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
 
 }]);
