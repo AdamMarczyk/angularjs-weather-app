3c431bd784d386a5d817844cc16e01cda627cae8
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-29 20:21:15 +0200

    setup controllers

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.controller('homeController', ['$scope', function ($scope) {
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function ($scope) {
+
+}]);
