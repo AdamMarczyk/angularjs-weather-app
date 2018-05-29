ff75fc78415eda4b2079efa5d95e550fc38d035b
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-29 21:24:20 +0200

    split app

---
+++ controllers.js
@@ -0,0 +1,31 @@
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.watch('city', function () {
+    cityService.city = $scope.city;
+
+  });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.days = $routeParams.days || 2;
+
+  $scope.weatherAPI = $resource("http://api.com", {
+    callback: "JSON_CALLBACK"
+  }, { get: { method: "JSONP" } });
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+
+  $scope.convertToCelsius = function (degK) {
+    return degK - 273;
+  };
+
+  $scope.convertToDate = function (dt) {
+    return new Date(dt * 1000);
+  };
+
+}]);
